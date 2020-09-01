bb42408eb69394e1f6f717fbcbcaf7a204ad1e74
Carlos Navarro <elgyro99@gmail.com>
2020-09-01 12:12:56 -0500

    Enemy Movement
    -Enemy Waypoints.
    -Waypoint prefabs.
    -Wave Config scriptable object

---
+++ HaloSpaceWars/Assets/Scripts/EnemyPath.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPath : MonoBehaviour
+{
+    [SerializeField] WaveConfig waveConfig;
+    List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPos = waypoints[waypointIndex].transform.position;
+            var movementFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPos, movementFrame);
+
+            if (transform.position == targetPos)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
